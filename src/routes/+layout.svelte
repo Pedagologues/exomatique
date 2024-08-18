693ebab770b7b7976ddaa2afcccf8c9a4ae537c1
Wyrdix <wyrdix@gmail.com>
2024-08-18 11:10:07 +0200

    Enable skeleton and tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { LightSwitch } from '@skeletonlabs/skeleton';
+  import { AppBar } from '@skeletonlabs/skeleton';
+  import "../app.css";
+</script>
+
+<div>
+  <AppBar>
+    <svelte:fragment slot="lead">
+        Exomatique  
+    </svelte:fragment>
+
+    <div class="flex flex-column gap-x-4 justify-around grow">
+    </div>
+    <svelte:fragment slot="trail">
+      <LightSwitch />
+    </svelte:fragment>
+  </AppBar>
+  <slot />
+</div>
